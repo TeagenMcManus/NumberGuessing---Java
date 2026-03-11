cde256ad8984848f2e292aa96e4d6e5efbfcfdb9
Teagen.A.M <88509144+TeagenMcManus@users.noreply.github.com>
2026-03-11 18:13:58 +0000

    Fully completed the user section and ironed out the bugs

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # NumberGuessing Java
+
+
+
+commands for me:
+git add .
+git commit -m "Describe your changes"
+git push origin main
+
+Runs the code
+javac guessing.java
+java guessing
--- guessing.java
+++ guessing.java
@@ -7,42 +7,46 @@ public class guessing {
 
     //Player guessing
     public static void playerGuessing(int admCheck) {
+        //Adds a gap
+        System.out.println("");
 
         //Allows for user input
-        Scanner userGuess = new Scanner(System.in);
+        Scanner user = new Scanner(System.in);
+        int userGuess = 0;
 
         //Grabs a random number
         Random rand = new Random();
         int number = rand.nextInt(100) + 1;
 
+        System.out.println("I'm thinking of a number between 1 and 100.");
+
         //Checks to see if the user did an admin check
         if (admCheck == 51) {
-            System.out.print("Computers number: " + number);
+            System.out.println("Computers number: " + number);
         };
 
         //Counts the attemps
         int attempts = 0;
 
-        System.out.println("I'm thinking of a number between 1 and 100.");
-
         while (userGuess != number) {
-
+            System.out.println("==============================");
+            
             //Ask / Grabs the users guess and adds to the attemps
             System.out.print("Whats your guess?: ");
-            userGuess = scanner.nextInt();
+            userGuess = user.nextInt();
             attempts++;
 
-            if (guess < number) {
+            if (userGuess < number) {
                 System.out.println("Too low!");
             } 
-            else if (guess > number) {
+            else if (userGuess > number) {
                 System.out.println("Too high!");
             } 
             else {
                 System.out.println("Correct! You guessed it in " + attempts + " tries.");
             }
+        }
     }
-
     //Main function / Menu
     public static void main(String[] args) {
 
@@ -51,21 +55,23 @@ public class guessing {
 
         //Title
         System.out.println("Welcome to the Number Guessing Game");
+        System.out.println("==============================");
 
         //Game selection
         System.out.println("1: Play against the computer");
         System.out.println("2: Computer manually plays against you");
-        System.out.println("3: Computer automattically plays against you")
+        System.out.println("3: Computer automattically plays against you");
 
         //Allows for the admin view
-        System.out.println("Admin View: (51: Can see the computers number), (53: Can see the computers guesses)")
-        
+        System.out.println("Admin View: (51: Can see the computers number), (53: Can see the computers guesses)");
+        System.out.println("==============================");
+
         //User ask to select mode
         System.out.print("Mode Selection(1, 2, 3, Admin View(51, 52)): ");
-        int userInp = userInp.nextInt();
+        int userInp = userInput.nextInt();
 
         //Checks to see what mode got selected
-        if (userInp == 1) {
+        if (userInp == 1 || userInp == 51 ) {
             playerGuessing(userInp);
         } 
         else if (userInp == 2) {
@@ -75,6 +81,6 @@ public class guessing {
             System.out.println("Invalid choice.");
         }
 
-        scanner.close();
+        userInput.close();
     }
 }
